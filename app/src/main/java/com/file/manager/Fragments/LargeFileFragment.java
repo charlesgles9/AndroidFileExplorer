62fba7d2717756f154c53bc4f5181a15bca4495f
tnt <charlesgles9@gmail.com>
2022-04-18 11:20:50 +0300

    ./solved bugs on LargeFileFragment

--- app/src/main/java/com/file/manager/Fragments/LargeFileFragment.java
+++ app/src/main/java/com/file/manager/Fragments/LargeFileFragment.java
@@ -1,6 +1,7 @@
 package com.file.manager.Fragments;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -351,6 +352,12 @@ public class LargeFileFragment extends Fragment implements IOnBackPressed {
                 folder.removeDeleted(data);
                 adapter.notifyDataSetChanged();
                 updateSelectedItemCount();
+            }
+        });
+        dialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            @Override
+            public void onCancel(DialogInterface dialog) {
+                adapter.notifyDataSetChanged();
             }
         });
         dialog.show();
